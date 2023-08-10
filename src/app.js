6474bd0e49c4b7053ae7c068a7c9122406c2f673
sysim <sysim@skyware.co.kr>
2023-08-10 14:53:21 +0900

    (webpack) IIFE 적용

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log(sum(1, 2))
+//IIFE 적용 전
+console.log('IIFE Before',sum(1, 2))
+
+//IIFE 적용
+console.log('IIFE', math.sum(1, 2))
--- src/math.js
+++ src/math.js
@@ -1,3 +1,17 @@
+//IIFE 사용
+var math = math || {}; //math가 있으면 할당하고 없으면 빈 객체를 할당한다.
+
+(function() {
+    //함수 내 스코프가 생긴다.
+    function sum(a, b) {
+        return a + b;
+    }
+    //외부에서 사용할 수 있도록 전역 변수에 할당한다.
+    math.sum = sum;
+    
+})()
+
+//IIFE 적용 전
 function sum(a, b) {
     return a + b
 }
