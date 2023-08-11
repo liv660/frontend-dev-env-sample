dd8067970b7e6de785f33d9a5c2753c196084436
sysim <liv660n@gmail.com>
2023-08-11 15:35:28 +0900

    로더 (로더의 기본 동작)

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,13 @@ module.exports = {
     output: {
         path: path.resolve('./dist'), //path 모듈로 절대경로를 사용한다.
         filename: '[name].js' //entry에서 설정한 이름으로 동적할당 된다. 여기서는 main.js로 생성된다.
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: path.resolve('./my-webpack-loader.js')
+            }
+        ]
     }
 }
