c714735af59dcbd9eb3ba26e0360abcf035a72ad
sysim <liv660n@gmail.com>
2023-08-18 15:37:57 +0900

    핫 로딩

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
-import axios from 'axios';
+import form from './form';
+import result from './result';
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const res = await axios.get('/api/users');
-    console.log(res);
+let resultEl;
+let formEl;
 
-    document.body.innerHTML = (res.data || []).map(user => {
-        return `<div>${user.id}: ${user.name}</div>`
-    }).join('');
+document.addEventListener("DOMContentLoaded", async () => {
+    formEl = document.createElement('div');
+    formEl.innerHTML = form.render();
+    document.body.appendChild(formEl);
+
+    resultEl = document.createElement('div');
+    resultEl.innerHTML = await result.render();
+    document.body.appendChild(resultEl);
 })
+
+if (module.hot) {
+    console.log('핫 모듈 켜짐')
+
+    //변경 감지 확인해보기
+    module.hot.accept('./result', async () => {
+        console.log('result 모듈 변경됨.')
+        resultEl.innerHTML = await result.render();
+    });
+
+    module.hot.accept('./form', () => {
+        formEl.innerHTML = form.render();
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         filename: '[name].js' //entry에서 설정한 이름으로 동적할당 된다. 여기서는 main.js로 생성된다.
     },
     devServer: {
+        //hot: true, //hot module replacement 기능 사용, webpack-dev-server 버전 4이상부터는 default 값이 true이다.
         client: {
             overlay: true,
         },
@@ -37,10 +38,10 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                use: path.resolve('./my-webpack-loader.js')
-            },
+            // {
+            //     test: /\.js$/,
+            //     use: path.resolve('./my-webpack-loader.js')
+            // },
             {
                 test: /\.css$/,
                 use: [
